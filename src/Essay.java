254ee706f9a3577b52523ed694bb7fc10df73c00
Elio-Benoît Sassine <2493512@edu.vaniercollege.qc.ca>
2025-08-29 12:50:22 -0400

    Add essay class

---
+++ src/Essay.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 2493512
+ */
+public class Essay {
+    private double grammar;
+    private double spelling;
+    private double correctLength;
+    private double content;
+    
+    
+
+    public void setScore(double gr, double sp, double len, double cnt) {
+        this.setGrammar(gr);
+        this.setSpelling(sp);
+        this.setCorrectLength(len);
+        this.setContent(cnt);
+    }
+    
+    public void setGrammar(double grammar) {
+        if (grammar > 30) {
+            System.out.println("Invalid score: Grammar score must be less than 30");
+            return;
+        }
+        this.grammar = grammar;
+    }
+
+    public void setSpelling(double spelling) {
+        if (spelling > 20) {
+            System.out.println("Invalid score: Spelling score must be less than 20");
+            return;
+        }
+        this.spelling = spelling;
+    }
+
+    public void setCorrectLength(double correctLength) {
+        if (correctLength > 20) {
+            System.out.println("Invalid score: Length score must be less than 20");
+            return;
+        }
+        this.correctLength = correctLength;
+    }
+
+    public void setContent(double content) {
+        if (content > 30) {
+            System.out.println("Invalid score: Content score must be less than 30");
+            return;
+        }
+        this.content = content;
+    }
+
+    public double getGrammar() {
+        return grammar;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+
+    public double getContent() {
+        return content;
+    }
+    
+    
+}
